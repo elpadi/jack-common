4639b32bb2755a4b1ad1aab767e2b9f299360549
carlos (el-cajon) <padi_05@yahoo.com>
2016-03-27 22:10:52 -0400

    Staging changes

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,13 +3,13 @@ use Jack\Jack;
 use Noodlehaus\Config;
 
 define('JACK_DIR', dirname(__DIR__));
-define('IS_LOCAL', in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', "::1")) || strpos($_SERVER['HTTP_HOST'], 'localhost') !== false);
+define('IS_LOCAL', false);
 
 require(JACK_DIR.'/vendor/autoload.php');
 require(JACK_DIR.'/config/propel/propel.php');
 Jack::$config = new Config(JACK_DIR.'/config/jack');
 
-defined('DEBUG') or define('DEBUG', IS_LOCAL || Jack::$config->get('debug'));
+define('DEBUG', false);
 function s($s) { return new Spatie\String\Str($s); }
 
 ini_set('display_errors', DEBUG ? 'on' : 'off');
