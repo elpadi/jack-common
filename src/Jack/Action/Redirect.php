24e49ab12c6dcef401461431ac50fd8ce23410c6
carlos <padi_05@yahoo.com>
2016-12-28 13:46:03 -0500

    Fixed error on redirects

--- src/Jack/Action/Redirect.php
+++ src/Jack/Action/Redirect.php
@@ -3,6 +3,10 @@ namespace Jack\Action;
 
 class Redirect {
 
+	public function __construct($route) {
+		$this->route = $route;
+	}
+
 	public function run($request, $response, $args) {
 		global $app;
 		return $response->withRedirect($app->routeLookup($this->route['action'], $args));
