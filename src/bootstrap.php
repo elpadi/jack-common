cbc85a509298664d4b8106aef0b53a8077783be7
carlos <padi_05@yahoo.com>
2016-12-28 13:45:02 -0500

    Error settings before all code

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,11 +3,12 @@ use Jack\Jack;
 
 define('JACK_DIR', dirname(__DIR__));
 define('IS_LOCAL', in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', "::1")) || strpos($_SERVER['HTTP_HOST'], 'localhost') !== false);
-
-require(JACK_DIR.'/vendor/autoload.php');
 defined('DEBUG') or define('DEBUG', isset($_ENV['debug']) ? ($_ENV['debug'] === 'true') : IS_LOCAL);
 
 ini_set('log_errors','on');
 ini_set('error_log', JACK_DIR.'/log/php_errors.log');
 ini_set('display_errors', DEBUG ? 'on' : 'off');
 error_reporting(DEBUG ? E_ALL : 0);
+
+require(JACK_DIR.'/vendor/autoload.php');
+
