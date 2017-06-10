762be62396b1ff6d75e5ae35dbd134d356a9a9b5
carlos (minimal) <padi_05@yahoo.com>
2017-06-09 20:29:31 -0400

    Using getenv over $_ENV

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,15 +3,12 @@ use Jack\Jack;
 
 define('JACK_DIR', dirname(__DIR__));
 define('IS_LOCAL', in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', "::1")) || strpos($_SERVER['HTTP_HOST'], 'localhost') !== false);
-defined('DEBUG') or define('DEBUG', isset($_ENV['debug']) ? ($_ENV['debug'] === 'true') : IS_LOCAL);
+defined('DEBUG') or define('DEBUG', getenv('DEBUG') === FALSE ? IS_LOCAL : getenv('DEBUG') === 'true');
 
 ini_set('log_errors','on');
 ini_set('error_log', JACK_DIR.'/log/php_errors.log');
 ini_set('display_errors', DEBUG ? 'on' : 'off');
 error_reporting(DEBUG ? E_ALL : 0);
-
-if (!isset($_ENV['CSS_VERSION'])) $_ENV['CSS_VERSION'] = '1';
-if (!isset($_ENV['JS_VERSION'])) $_ENV['JS_VERSION'] = '1';
 
 require(JACK_DIR.'/vendor/autoload.php');
 
