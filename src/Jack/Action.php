c1ea183fa9c10cea6207e434f137c633aa839806
carlos (minimal) <padi_05@yahoo.com>
2017-06-17 21:11:46 -0400

    Uppercase namespace for action url section

--- src/Jack/Action.php
+++ src/Jack/Action.php
@@ -7,7 +7,7 @@ class Action {
 		if (isset($route['action']) && is_string($route['action'])) return new Action\Redirect($route);
 		$actionName = str_replace(' ', '', ucwords(preg_replace('/[^a-z]/', ' ', $route['name'])));
 		foreach (App::namespaces() as $ns) {
-			$classname = '\\'.$ns.'\Action\\'.(isset($group['name']) ? "$group[name]\\" : '').$actionName;
+			$classname = '\\'.$ns.'\Action\\'.(isset($group['name']) ? ucfirst("$group[name]\\") : '').$actionName;
 			if (class_exists($classname)) return new $classname($route);
 		}
 		foreach (App::namespaces() as $ns) {
